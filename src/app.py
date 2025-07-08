85e2321c2e97c9605dc7ca3245e9ea28a10a9d03
livialop <livvialn7@gmail.com>
2025-07-08 17:36:51 -0300

    footer

--- src/app.py
+++ src/app.py
@@ -1,4 +1,9 @@
+from flask import *
+
 import sqlite3
+
+# Fazendo a aplicação
+app = Flask(__name__)
 
 
 # Abrindo uma conexão
@@ -10,6 +15,12 @@ SCHEMA = 'schema.sql'
 # Para executar isso, é preciso estar localizado na pasta models
 with open(SCHEMA) as f:
     conn.executescript(f.read())
+
+# Primeira rota. Tentando visualizar o footer e posteriormente a header
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 # Fechando a conexão
 conn.close()
