c6bbfea2b7e2253a393c567e9d10ae429f3a82f6
livialop <livvialn7@gmail.com>
2025-07-28 21:57:47 -0300

    revert

--- src/schema.sql
+++ src/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL,
     email VARCHAR(120) NOT NULL UNIQUE,
     password_hash VARCHAR(120) NOT NULL,
-    profile_pic VARCHAR(255) DEFAULT NULL, -- Caminho para a foto de perfil do usuário
+    profile_pic TEXT DEFAULT NULL, -- Caminho para a foto de perfil do usuário
     data_created DATETIME DEFAULT (datetime('now', 'localtime'))
 );
 
